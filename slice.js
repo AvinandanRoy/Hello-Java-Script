9a26d1c2ae1d233502059a6e9711292e459a90d8
Avinandan Roy <abhinandanroylipi@gmail.com>
2023-08-02 23:56:54 +0600

    splice concept id clear boom boom :)

--- slice.js
+++ slice.js
@@ -1,4 +1,6 @@
 // sliceing an array ---->
+// it doesn't change the original array
+
 const slicingArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20];
 
 const newArray = slicingArray.slice(5 , 13 );
