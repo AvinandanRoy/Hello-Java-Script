5b4043399fc4b1548dd8e9c845690ab655883445
Avinandan Roy <abhinandanroylipi@gmail.com>
2023-08-11 08:52:30 +0600

    done perfectly

--- simple1.js
+++ simple1.js
@@ -1,5 +1,7 @@
 // decreasing
-
+let count = 0;
 for(let i = 5 ; i >= 0 ; i--){
-    console.log(i);
+    console.log("Number  of",count,"index is",i);
+    count++;
 }
+console.log("Total element is ", count);
